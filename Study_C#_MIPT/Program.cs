5f451b090aef48013ae102b0ddeef5e8cd81d4e0
Aleksandra Nikolova <nikolova.alexandra@mail.ru>
2024-06-12 18:17:43 +0300

    start project. V0.1

---
+++ Study_C#_MIPT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Study_C__MIPT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
